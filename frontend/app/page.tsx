3ffeee588aa996d8270cc6e47de88e1b94979a97
julyperfect07 <abdallah.july05@gmail.com>
2026-06-09 15:23:40 +0300

    handle the file share fucntionality

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,19 +9,42 @@ export default function Home() {
   const { socket } = useSocket();
   const connections = useRef<DataConnection[]>([]);
   const [file, setFile] = useState<File | null>();
-  const handleData = () => {};
+  const handleData = (data: unknown) => {
+    if (typeof data === "object" && data !== null && "type" in data) {
+      const d = data as {
+        type: string;
+        name: string;
+        fileType: string;
+        data: ArrayBuffer;
+      };
+      if (d.type === "file") {
+        const blob = new Blob([d.data], { type: d.fileType });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = d.name;
+        a.click();
+
+        URL.revokeObjectURL(url);
+      }
+    }
+  };
 
   useEffect(() => {
     if (!socket || !peerId || !peer) return;
     socket?.emit("join-room", { roomId: "123", peerId });
 
-    // If i am the first one this one triggers , but if i was already found the new peer wants to connect to me with peer.connect and this fires peer.on("connection")
+    // user-connected fires when someone ELSE joins after me
+    // meaning i am already in the room, THEY just arrived
+    // so i connect to THEM
     socket.on("user-connected", ({ peerId: otherPeerId }) => {
       const conn = peer.connect(otherPeerId);
       connections.current?.push(conn);
       conn.on("data", handleData);
     });
 
+    // this fires when i am the newcomer
+    // and the existing user called peer.connect(yourPeerId)
     peer.on("connection", (conn) => {
       connections.current?.push(conn);
       conn.on("data", handleData);
@@ -43,9 +66,24 @@ export default function Home() {
       setFile(e.target.files[0]);
     }
   };
+
+  const sendFile = async () => {
+    if (!file) return;
+    const buffer = await file.arrayBuffer();
+    connections.current.forEach((conn) => {
+      conn.send({
+        type: "file",
+        name: file.name,
+        fileType: file.type,
+        data: buffer,
+      });
+    });
+    setFile(null);
+  };
   return (
     <div>
       <input type="file" onChange={handleFileChange} />
+      <button onClick={sendFile}>Send</button>
     </div>
   );
 }
